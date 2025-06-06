3cf0ae05eb572b0c7880109f9a932c8ee603b180
rpalmier <rpalmier@et.intechinfo.fr>
2025-06-06 17:25:04 +0200

    update MLP

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-from mylib import sum_vector
+from mylib import Perceptron
 
-print(sum_vector([1.0, 2.0, 3.0]))  # attend 6.0
+# Table de vérité du AND logique
+X = [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
+Y = [-1.0, -1.0, -1.0, 1.0]  # cible: -1 sauf pour [1.0, 1.0]
+
+model = Perceptron()
+model.fit(X, Y, iterations=10)
+
+for x in X:
+    print(f"{x} → {model.predict(x)}")
